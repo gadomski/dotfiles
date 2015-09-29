b91404afc0e41d3e9533b09f68227d1c40ed9701
Pete Gadomski <pete.gadomski@gmail.com>
2015-09-29 09:18:33 -0500

    Add some custom debian build commands

--- oh-my-zsh-custom/debian.zsh
+++ oh-my-zsh-custom/debian.zsh
@@ -2,3 +2,5 @@ export DEBFULLNAME="Pete Gadomski"
 export DEBEMAIL="pete.gadomski@gmail.com"
 
 alias gbpp="gbp buildpackage --git-pbuilder"
+alias gbpq="gbp buildpackage -us -uc -b"
+alias gbps="gbp buildpackage -S -sa"
