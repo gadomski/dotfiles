03ac88480cb24b27c4cf1d5ecb4fad9409860aeb
Pete Gadomski <pete.gadomski@gmail.com>
2015-10-01 20:55:58 -0500

    Add documents alias

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -1,4 +1,5 @@
 r=$HOME/Repos
+d=$HOME/Documents
 
 alias nb="ninja -C build"
 alias cmakeinit="(mkdir build && cd build && ccmake .. -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$HOME/local)"
