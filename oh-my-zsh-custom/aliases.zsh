da783f65b0bc7c7742fddbd504d47238085433d2
Pete Gadomski <pete.gadomski@gmail.com>
2015-10-06 09:49:34 -0500

    Add ccb alias

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -2,4 +2,5 @@ r=$HOME/Repos
 d=$HOME/Documents
 
 alias nb="ninja -C build"
+alias ccb="(cd build && ccmake ..)"
 alias cmakeinit="(mkdir build && cd build && ccmake .. -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$HOME/local)"
