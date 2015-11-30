ec99a72140003ed2f60eaa3021e6297babdafae6
Pete Gadomski <pete.gadomski@gmail.com>
2015-11-30 06:10:41 -0600

    Add git lfs shortcut

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -4,3 +4,4 @@ d=$HOME/Documents
 alias nb="ninja -C build"
 alias ccb="(cd build && ccmake ..)"
 alias cmakeinit="(mkdir build && cd build && ccmake .. -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$HOME/local -DCMAKE_EXPORT_COMPILE_COMMANDS=ON)"
+alias glfs="git lfs"
