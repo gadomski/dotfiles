2f85c108cf3b40d0c1416aee5f22401b14668b40
Pete Gadomski <pete.gadomski@gmail.com>
2016-12-01 06:03:04 -0700

    Add custom commands

--- custom/cmake.zsh
+++ custom/cmake.zsh
@@ -1,2 +1,3 @@
+alias cmakeinit="(mkdir -p build && cd build && cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/local -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..)"
 alias ccb="ccmake --build build"
 alias nb="ninja -C build"
