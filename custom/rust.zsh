190a03897b72c7f374645e8fea8b9fd9b691f579
Pete Gadomski <pete.gadomski@gmail.com>
2017-01-04 10:38:01 -0700

    Some rust changes

--- custom/rust.zsh
+++ custom/rust.zsh
@@ -1,2 +1,4 @@
 source $HOME/.cargo/env
 export RUST_SRC_PATH=$HOME/.multirust/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src
+alias clippy="rustup run nightly cargo clippy"
+alias c="cargo"
