3089423dd302eca3f661fbd3f0ac4f46b70773dd
Pete Gadomski <pete.gadomski@gmail.com>
2017-04-07 09:51:48 -0600

    Add another testing directory to watchman ninja

--- custom/watchman.zsh
+++ custom/watchman.zsh
@@ -1,1 +1,1 @@
-alias watchman-ninja="watchman-make --make 'ninja -C build' -p '**/*.cpp' '**/*.hpp' CMakeLists.txt -t all -p 'test/**' -t test"
+alias watchman-ninja="watchman-make --make 'ninja -C build' -p '**/*.cpp' '**/*.hpp' CMakeLists.txt -t all -p 'tests/**' 'test/**' -t test"
