8e5d4722fc2e7a6843b45c297ebe5b64ccb193ae
Pete Gadomski <pete.gadomski@gmail.com>
2017-04-19 15:17:11 -0600

    Add twitter tiny care terminal

---
+++ custom/ttc.zsh
@@ -0,0 +1,20 @@
+# List of accounts to read the last tweet from, comma separated
+# The first in the list is read by the party parrot.
+export TTC_BOTS='tinycarebot,selfcare_bot,magicrealismbot'
+
+# List of folders to look into for `git` commits, comma separated.
+export TTC_REPOS='~/Repos/Code'
+
+# Location/zip code to check the weather for. Both 90210 and "San Francisco, CA"
+# should be ok. It's using weather.service.msn.com behind the curtains.
+export TTC_WEATHER='80503'
+
+# Set to false if you're an imperial savage. <3
+export TTC_CELSIUS=true
+
+# Unset this if you _don't_ want to use Twitter keys and want to
+# use web scraping instead.
+export TTC_APIKEYS=true
+
+# Refresh the dashboard every 20 minutes.
+export TTC_UPDATE_INTERVAL=20
