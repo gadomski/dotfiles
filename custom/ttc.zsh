7c071468e432a3c8f86c525160294a9960ccafa5
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-23 11:55:07 -0600

    Update tiny-care-terminal config

--- custom/ttc.zsh
+++ custom/ttc.zsh
@@ -2,12 +2,14 @@
 # The first in the list is read by the party parrot.
 export TTC_BOTS='tinycarebot,selfcare_bot,magicrealismbot'
 
+TTC_SAY_BOX='panda'
+
 # List of folders to look into for `git` commits, comma separated.
-export TTC_REPOS='~/Repos/Code'
+export TTC_REPOS='~/Repos'
 
 # Location/zip code to check the weather for. Both 90210 and "San Francisco, CA"
 # should be ok. It's using weather.service.msn.com behind the curtains.
-export TTC_WEATHER='80503'
+export TTC_WEATHER='80501'
 
 # Set to false if you're an imperial savage. <3
 export TTC_CELSIUS=true
